6309a04d22710be5d3c6a1e062ed28681329c84c
John McGovern <johnmcg7@gmail.com>
2022-08-23 23:26:23 -0700

    First run at Time Compression functionality

--- main.py
+++ main.py
@@ -17,19 +17,27 @@ splunkHecToken = "9802541d-394f-4053-b973-306757e15ed3"
 splunkIndex = "test"   # Currently we always override the data files index specification (but this may change)
 splunkAuthHeader = {'Authorization': 'Splunk {}'.format(splunkHecToken)}
 
-speedUpFactor = 0
+speedUpFactor = 2
+speedUpInterval = 5000
 
 shouldLoop = False
 
 debug = False
 
-stateTracker = {"currentLine": 1, "timeOffset": 0.0, "speedUpOffset": 0 }
-stateFilePath = "./var/sample.state"
+stateTracker = {"currentLine": 1, 
+                "timeOffset": 0.0,
+                "timeDelta": 0.0, 
+                "speedUpOffset": 0 }
+stateFilePath = "./var/pontoon.state"
+stateTrackerReportingFactor = 2000
+stateTrackerModFactor = 100
 
-print("State File Location: ", stateFilePath)
+
 print("Data File Location: ", dataFilePath)
+print("State File Location: ", stateFilePath)
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 # Check if the state file exists and load if so
 if os.path.exists(stateFilePath): 
@@ -71,48 +79,70 @@ if os.path.exists(dataFilePath):
 
 
 # Main loop
-#while int(stateTracker['currentLine']) <= int(dataFileLength):
-while 1==1:
-    # Get one line at a time from the data file
-    lineData = linecache.getline(dataFilePath, int(stateTracker['currentLine']))
-    currentLineJson = json.loads(lineData)
+print("Begin Main Loop")
 
-    stateTracker['speedUpOffset'] += speedUpFactor
+try:
+    while 1==1:
+        # Get one line at a time from the data file
+        lineData = linecache.getline(dataFilePath, int(stateTracker['currentLine']))
+        currentLineJson = json.loads(lineData)
 
-    if debug:
-        print("Line", int(stateTracker['currentLine']),"Time Delta: ", time.time() - (float(currentLineJson['time']) + float(stateTracker['timeOffset']) + float(stateTracker['speedUpOffset'])))
+        if stateTracker['currentLine'] % speedUpInterval == 0:
+            stateTracker['speedUpOffset'] += speedUpFactor
 
-    if (float(currentLineJson['time']) + float(stateTracker['timeOffset']) + float(stateTracker['speedUpOffset'])) <= time.time():
-        eventJson = {"time": time.time(), "index": splunkIndex, "host":currentLineJson['host'], "source":currentLineJson['source'], "sourcetype":currentLineJson['sourcetype'],  "event": currentLineJson['event'] }
-        r = requests.post(splunkUrl, headers=splunkAuthHeader, json=eventJson, verify=False)
+        stateTracker['timeDelta'] = (time.time() + float(stateTracker['speedUpOffset'])) - (float(currentLineJson['time']) + float(stateTracker['timeOffset']))
+
         if debug:
-            print("Sent Line: ", stateTracker['currentLine'])
+            print("Line", int(stateTracker['currentLine']), "Time Delta:", stateTracker['timeDelta'])
 
-        file = open(stateFilePath, "w")
-        json.dump(stateTracker, file)
-        file.close()
+        if stateTracker['timeDelta'] >= 0:
+            eventJson = {"time": time.time(), 
+                        "index": splunkIndex, 
+                        "host":currentLineJson['host'], 
+                        "source":currentLineJson['source'], 
+                        "sourcetype":currentLineJson['sourcetype'],  
+                        "event": currentLineJson['event'] }
+            r = requests.post(splunkUrl, headers=splunkAuthHeader, json=eventJson, verify=False)
+            if debug:
+                print("Sent Line: ", stateTracker['currentLine'])
 
-        # time.sleep(.001)
 
-        if int(stateTracker['currentLine']) == int(dataFileLength) and shouldLoop==True:
-            print("Reached EoF - Starting Over ", stateTracker)
-            stateTracker['currentLine'] = 1
-            lineData = linecache.getline(dataFilePath, int(stateTracker['currentLine']))
-            currentLineJson = json.loads(lineData)
-            stateTracker['timeOffset'] = time.time() - float(currentLineJson['time'])
-            stateTracker['speedUpOffset'] = 0
-            print("State Reset Completed", stateTracker)
-    
-        if int(stateTracker['currentLine']) == int(dataFileLength) and shouldLoop==False:
-            print("Reached EoF - Deleting State File")
-            os.remove(stateFilePath)
-            print("Reached EoF - Quitting")
-            exit()    
-    
-        stateTracker['currentLine'] += 1
+            if stateTracker['currentLine'] % stateTrackerReportingFactor == 0:
+                print("State Tracker:", stateTracker)
 
-    else:
-        # Sleep almost a second
-        time.sleep(.95)
-    
+            if stateTracker['currentLine'] % stateTrackerModFactor == 0:
+                file = open(stateFilePath, "w")
+                json.dump(stateTracker, file)
+                file.close()
 
+            # If we reach EoF and shouldLoop==True, then reset the stateTracker and start over.
+            if int(stateTracker['currentLine']) == int(dataFileLength) and shouldLoop==True:
+                print("Reached EoF - Starting Over ", stateTracker)
+                stateTracker['currentLine'] = 1
+                lineData = linecache.getline(dataFilePath, int(stateTracker['currentLine']))
+                currentLineJson = json.loads(lineData)
+                stateTracker['timeOffset'] = time.time() - float(currentLineJson['time'])
+                stateTracker['speedUpOffset'] = 0
+                print("State Reset Completed", stateTracker)
+
+            # If we reach EoF and shouldLoop==False, then delete the state file and exit.
+            if int(stateTracker['currentLine']) == int(dataFileLength) and shouldLoop==False:
+                print("Reached EoF - Deleting State File")
+                os.remove(stateFilePath)
+                print("Reached EoF - Quitting")
+                exit()    
+        
+
+            stateTracker['currentLine'] += 1
+
+        else:
+            time.sleep(.25)
+
+except KeyboardInterrupt:
+    file = open(stateFilePath, "w")
+    json.dump(stateTracker, file)
+    file.close()
+
+    print("Caught Keyboard Interrupt - Quitting")
+    print("State Tracker:", stateTracker)
+        
