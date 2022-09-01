2688ef847062183e27e436c01bacff712382f2e2
John McGovern <johnmcg7@gmail.com>
2022-08-31 21:39:27 -0700

    Updated config-sample.py documentation

--- config-sample.py
+++ config-sample.py
@@ -3,12 +3,14 @@
 #
 
 # The name of the file to use. ".json" is assumed unless modified below.
+# file_key can be passed as the first command line argument (i.e. main.py filekey)
 file_key = "botsv4"
 data_file_path = "./data/" + file_key + ".json"
 state_file_path = "./var/" + file_key + ".state"
 
 
 # Splunk URL, HEC Token, and Index to use.
+# splunk_index can be passed as the second command line argument (i.e. main.py indexname)
 splunk_url = "https://example.splk.me:8088/"
 splunk_hec_token = "9802541d-394f-4053-b973-306757e15ed3"
 splunk_index = "test"
